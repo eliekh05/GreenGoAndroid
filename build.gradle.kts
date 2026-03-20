6cc4af4e199b2d39566a07ba0a954bab080b2d5c
GreenGo Fix <fix@greengo.app>
2026-03-20 08:24:54 +0000

    SonarQube Gradle plugin + fix set-output deprecation with ilharp/sign-android-release

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android)      apply false
     alias(libs.plugins.kotlin.compose)      apply false
+    id("org.sonarqube") version "7.2.3.7755"
 }
