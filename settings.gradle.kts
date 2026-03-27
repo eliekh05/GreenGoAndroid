a4ec3afbd0a5cf2e722520a7587c59877c598610
Elie Khalil <eliekh05@gmail.com>
2026-03-27 14:55:02 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,3 @@
-plugins {
-    id("com.gradle.develocity") version("4.4.0")
-}
-
-develocity {
-    buildScan {
-        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
-        termsOfUseAgree.set("yes")
-    }
-}
-
 pluginManagement {
     repositories {
         google {
@@ -20,6 +9,17 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.develocity") version("4.4.0")
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
     }
 }
 
